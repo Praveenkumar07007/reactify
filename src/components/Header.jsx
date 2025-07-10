9c4411bb6d381a4b6ac0aaec28adbacf57729d16
Praveen Kumar <115071711+Praveenkumar07007@users.noreply.github.com>
2025-07-10 19:48:28 +0530

    asd

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,10 @@
 import react from 'react'
 const Header = () => {
     return (
-        <h1>this is a header a components</h1>
+        <>
+            <h1>this is</h1>
+            <h2>this is a header a components</h2>
+        </>
     )
 }
 export default Header;
